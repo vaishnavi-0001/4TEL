a0760b0917567ad7dc30cf899b23853850e8fdae
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2024-04-14 13:53:37 +0530

    Add files via upload

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,43 +1,43 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     var countdownElement = document.getElementById("countdown");
-    var seconds = 10;
+    var seconds = 13;
 
     var countdownInterval = setInterval(function() {
         countdownElement.textContent = seconds;
-        
+    
         seconds--;
 
         if (seconds < 0) {
             clearInterval(countdownInterval);
-            countdownElement.style.opacity = "0"; // Hide the countdown when it reaches 0
-            slideUpSecondVideo();
+            slideUpSecondPage(); 
         }
     }, 1000);
+
+    
+    var menuToggle = document.querySelector('.menu-toggle');
+    menuToggle.addEventListener('click', toggleMenu);
 });
 
-function slideUpSecondVideo() {
-    // Get reference to the first video
-    const firstVideo = document.querySelector('.background-frontpage');
+function slideUpSecondPage() {
+   
+    var countdownElement = document.getElementById("countdown");
+    countdownElement.style.opacity = "0";
 
-    // Delay stopping the first video at 6 seconds
+    var content = document.querySelector('.content');
+    var sidebar = document.querySelector('.sidebar');
+    content.style.transition = "transform 1s ease";
+    content.style.transform = "translateY(-100%)";
+    sidebar.style.transition = "transform 1s ease";
+    sidebar.style.transform = "translateY(-100%)";
+
+    var secondPageOverlay = document.getElementById("secondPageOverlay");
+    secondPageOverlay.style.display = "block";
+
     setTimeout(function() {
-        // Pause the first video
-        firstVideo.pause();
-
-        // Slide up the first video
-        firstVideo.style.transition = "transform 1s ease";
-        firstVideo.style.transform = "translateY(-100%)";
-
-        // Delay showing the second video by 1 second
-        setTimeout(function() {
-            // Show the second video
-            const secondVideo = document.querySelector('.background-frontpage');
-            secondVideo.src = 'image/video_2.mp4';
-            secondVideo.style.transition = "transform 1s ease";
-            secondVideo.style.transform = "translateY(0)";
-            secondVideo.play();
-        }, 1000); // Delay showing the second video after sliding up the first video
-    }, 3000); // 6 seconds delay before stopping the first video and sliding up the second video
+        
+        window.location.href = "3_card.html";
+    }, 1000);
 }
 
 function toggleMenu() {
